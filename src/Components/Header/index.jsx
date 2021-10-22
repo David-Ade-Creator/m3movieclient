210e00ce62856b90db068424526043e3623f0548
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-10-23 02:54:21 +0530

    completed design for banner and navbar, yet to design the rows components

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function PageHeader({children}) {
+    return (
+        <React.Fragment>
+        <header class="header" id="header">
+            <div className="header_container container">
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+
+            <div class="header__img">
+                <img src="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" alt=""/>
+            </div>
+            </div>
+        </header>
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="/" class="nav__logo">
+                        <i class='bx bx-layer nav__logo-icon'></i>
+                        <span class="nav__logo-name">M3Movies</span>
+                    </a>
+
+                    <a href="/" class="nav__link">
+                    <i class='bx bx-bookmark nav__icon' ></i>
+                            <span class="nav__name">Favourites</span>
+                        </a>
+
+                    <div class="nav__list">
+                        <a href="/" class="nav__link active">
+                      
+                            <span class="nav__name">Home</span>
+                        </a>
+
+                        <a href="/" class="nav__link">
+                           
+                            <span class="nav__name">Anime</span>
+                        </a>
+                        
+                        <a href="/" class="nav__link">
+                          
+                            <span class="nav__name">Dramas</span>
+                        </a>
+
+                        <a href="/" class="nav__link">
+                          
+                            <span class="nav__name">Thrillers</span>
+                        </a>
+
+                        <a href="/" class="nav__link">
+                            <span class="nav__name">Independent Films</span>
+                        </a>
+
+                        <a href="/" class="nav__link">
+                            <span class="nav__name">Horror Films</span>
+                        </a>
+                    </div>
+                </div>
+
+                <a href="/" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Log Out</span>
+                </a>
+            </nav>
+        </div>
+        <main className="body-pd">
+        {children}
+        </main>
+        </React.Fragment>
+    )
+}
+
+export default PageHeader
--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Space } from "antd";
+import PageHeader from "Components/Header";
+import React from "react";
 
 function HomePage() {
-    return (
-        <div>
-            this is the homepage
+  return (
+    <PageHeader>
+      <div className="homepage_container container">
+        <div className="homepage_banner">
+          <div className="homepage_img">
+            <img
+              src="https://theenvoyweb.com/wp-content/uploads/2021/06/The-Witcher-2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="homepage_description">
+              <h2 className="homepage_title">The Witcher</h2>
+              <p>This is the fast and furios description and to be honest i don't even know what to write but anyway i will just do my best and see if it is fine...</p>
+              <div>
+                  <Space>
+                      <span className="button_primary">Details</span>
+                      <span className="button_default"><i class='bx bx-plus' ></i></span>
+                  </Space>
+              </div>
+          </div>
         </div>
-    )
+      </div>
+    </PageHeader>
+  );
 }
 
-export default HomePage
+export default HomePage;
